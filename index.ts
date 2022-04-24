c3548b82fdc917f16aba107b89ba2ee457a6b9a2
Eyyub <eyyub.erenn@gmail.com>
2022-04-24 19:18:07 +0300

    init

--- index.ts
+++ index.ts
@@ -5,7 +5,6 @@ export function helloWorld(names: Array<string>): string {
   return names.map<string>(name => 'hello ' + name).join(` `)
 }
 
-
 // read the given key from account (contract) storage
 export function read(key: string): string {
   if (storage.hasKey(key)) {

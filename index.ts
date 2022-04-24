fabffcfa3e37469fb947b3a2b3d3d0e7660a5982
Eyyub <eyyub.erenn@gmail.com>
2022-04-24 18:55:16 +0300

    master

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 import { storage, Context } from "near-sdk-as"
 
 // return the string 'hello world'
-export function helloWorld(names: Array<string>): string {  
-  return names.map<string>( name => 'hello' + name).join('')
+export function helloWorld(names: Array<string>): string {
+  return names.map<string>(name => 'hello ' + name).join(` `)
 }
 
 
